b06c55e61c0db4a4f6aa832ab4c9e7e73a3527ef
Sergio Rodriguez <arodriguezloza@yahoo.com.mx>
2020-07-01 22:00:50 -0500

    Adding a Hamburger Icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Aux';
-import classes from './Layout.css';
+import Aux from "../../hoc/Aux";
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, sideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer:false})
+
+    }
+
+    drawerToggleHandler= () => {
+        this.setState((prevState) => {
+            return {showSideDrawer:!prevState.showSideDrawer}
+        });
+    }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked = {this.drawerToggleHandler}/>
+        <SideDrawer 
+            open={this.state.showSideDrawer} 
+            closed={this.SideDrawerClosedHandler}/>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
